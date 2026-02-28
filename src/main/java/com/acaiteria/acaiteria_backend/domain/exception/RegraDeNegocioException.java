d94a29e1098f4b14ca73ae6099807c1dd9a5ce63
PedroHeitor12567 <pedroheitorrdac@gmail.com>
2026-02-28 17:00:32 -0300

    feat: criando exceptions

---
+++ src/main/java/com/acaiteria/acaiteria_backend/domain/exception/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package com.acaiteria.acaiteria_backend.domain.exception;
+
+public class RegraDeNegocioException extends RuntimeException {
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
